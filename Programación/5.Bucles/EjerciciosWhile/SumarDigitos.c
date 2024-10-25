26686baad10dab69c2713844b1bf55000ae3726c
marcos1304E <marcosescamilla1304@gmail.com>
2024-10-25 14:30:35 +0200

    CommitDel25

---
+++ Programación/5.Bucles/EjerciciosWhile/SumarDigitos.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+	int n;
+	int suma;
+		printf("Introduce un numero entero:\n");
+		scanf("%d",&n);
+		
+		while(n){
+
+
+
+
+
+
+		}
+
+	return 0;
+		}
