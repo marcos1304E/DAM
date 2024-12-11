326c771c7abef86aba5812199e71d8c5442bac8b
marcos1304E <marcosescamilla1304@gmail.com>
2024-12-11 11:19:05 +0100

    p6

--- Programación/Punteros/P6_Gestión_Biblioteca/P6_GestionBiblioteca_MarcosEscamilla.c
+++ Programación/Punteros/P6_Gestión_Biblioteca/P6_GestionBiblioteca_MarcosEscamilla.c
@@ -13,6 +13,7 @@ Programación
 Practica 6 -- Gestión de biblioteca
 */ //mas comentarios
 
+
 typedef enum{ //Enum para que se definan directamente, equivaldria a poner arriba varios defines con #define FICCION 0, por ejemplo, pero mucho más sencillo así y nos podemos entender para que abajo en el codigo si podamos poner la palabra literal FICCION, si no tendríamos que poner un 0.
 	FICCION, // 0
 	NO_FICCION, // 1
