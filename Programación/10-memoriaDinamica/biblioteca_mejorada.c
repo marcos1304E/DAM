1c06435cb403123e8a6d532df86a883debed7a3b
marcos1304E <marcosescamilla1304@gmail.com>
2025-01-08 12:41:18 +0100

    vuelta clases(biblio)

--- Programación/10-memoriaDinamica/biblioteca_mejorada.c
+++ Programación/10-memoriaDinamica/biblioteca_mejorada.c
@@ -54,14 +54,13 @@ typedef struct{
 
 
 //Ponemos const para que no se cambie, siempre el mismo sin modificar
-void printBook(const Libro * puntero_a_un_unico_libro){//Muestra info de libro al que se le apunte
+void printBook(Libro * dir, int id,char *titulo, char* autor,float precio, int categoria, int cantidad){//Muestra info de libro al que se le apunte
         //Con todas las propiedades del libro, id titulo, autor, precio, genero y cantidad
-        printf("Id: %d\n",puntero_a_un_unico_libro->id);
-        printf("Titulo: %s\n",puntero_a_un_unico_libro->titulo);
-        printf("Autor: %s\n",puntero_a_un_unico_libro->autor);
-        printf("Precio: %.2f\n",puntero_a_un_unico_libro->precio);
-        printf("Genero: %d\n",puntero_a_un_unico_libro->categoria);
-        printf("Cantidad: %d\n",puntero_a_un_unico_libro->cantidad);
+        dir -> id = id;
+        dir ->precio = precio;
+        dir -> categoria = categoria;
+        strcpy(dir-> autor, autor);
+        strcpy(dir-> titulo, titulo);
 
 }
 
@@ -71,8 +70,7 @@ void libroPorId(const Libro * catalogo, int id){
         for(int i=0; i <= CAT_MAX; i++){ //Bucle para que recorra todos los libros hasta el id que el usuario introduzca, y ahi imprimir el libro con la id que se ha introducido
             if(catalogo[i].id == id){ //Por eso hemos recorrido todo con el bucle for, y hemos llamado a la funcion printBook, en vez de hacer un printf para que imprima el libro completo, funcion que hemos hecho arriba, con todos los printf de todas las propiedades del libro.
             printBook(&catalogo[i]);
-            
-            
+                       
 
         }
 }
@@ -121,56 +119,71 @@ int main(int argc, char ** argv){ //int main(int argc, char ** argv){}...
             //argc: número de argumentos recibidos
             //argv: array de cadenas de texto
 
-        int numero_libros, id, menu;
+        int numero_libros, id, menu,cantidad;
 
         /*char palabra[10] = "Hola";
         printf("%c\n",*(palabra+1));*/
 
-        Libro libros [40] = { //array de los libros
+        Libro * libros = (Libro*) malloc(cantidad * sizeof(Libro));
 
-        {1, "To Kill a Mockingbird", "Harper Lee", 15.99,FICCION, 10},
-        {2, "1984", "George Orwell", 12.49,FICCION, 5},
-        {3, "The Great Gatsby", "F. Scott Fitzgerald", 10.99,FICCION, 8},
-        {4, "Moby Dick", "Herman Melville", 18.99,FICCION, 12},
-        {5, "War and Peace", "Leo Tolstoy", 20.00,FICCION, 7},
-        {6, "Pride and Prejudice", "Jane Austen", 14.99,FICCION, 9},
-        {7, "The Catcher in the Rye", "J.D. Salinger", 10.00,FICCION, 6},
-        {8, "The Odyssey", "Homer", 17.49,FICCION, 4},
-        {9, "Ulysses", "James Joyce", 25.00,FICCION, 2},
-        {10, "The Divine Comedy", "Dante Alighieri", 22.00, POESIA, 3},
-        {11, "Leaves of Grass", "Walt Whitman", 13.00, POESIA, 11},
-        {12, "The Iliad", "Homer", 18.50,FICCION, 7},
-        {13, "A Brief History of Time", "Stephen Hawking", 15.00, NO_FICCION, 15},
-        {14, "The Art of War", "Sun Tzu", 9.99, NO_FICCION, 20},
-        {15, "Sapiens: A Brief History of Humankind", "Yuval Noah Harari", 16.49, NO_FICCION, 13},
-        {16, "The Selfish Gene", "Richard Dawkins", 14.00, NO_FICCION, 6},
-        {17, "The Road to Serfdom", "F.A. Hayek", 10.50, NO_FICCION, 5},
-        {18, "The Wealth of Nations", "Adam Smith", 30.00, NO_FICCION, 8},
-        {19, "On the Origin of Species", "Charles Darwin", 24.99, NO_FICCION, 4},
-        {20, "The Prince", "Niccolò Machiavelli", 8.99, NO_FICCION, 14},
-        {21, "Hamlet", "William Shakespeare", 11.50, TEATRO, 6},
-        {22, "Macbeth", "William Shakespeare", 9.50, TEATRO, 8},
-        {23, "Othello", "William Shakespeare", 10.99, TEATRO, 7},
-        {24, "A Doll's House", "Henrik Ibsen", 12.50, TEATRO, 5},
-        {25, "Waiting for Godot", "Samuel Beckett", 13.99, TEATRO, 4},
-        {26, "Death of a Salesman", "Arthur Miller", 14.99, TEATRO, 10},
-        {27, "The Glass Menagerie", "Tennessee Williams", 11.00, TEATRO, 9},
-        {28, "Long Day's Journey into Night", "Eugene O'Neill", 19.50, TEATRO, 3},
-        {29, "The Importance of Being Earnest", "Oscar Wilde", 8.50, TEATRO, 15},
-        {30, "The Waste Land", "T.S. Eliot", 6.99, POESIA, 10},
-        {31, "Paradise Lost", "John Milton", 12.00, POESIA, 7},
-        {32, "Beowulf", "Anonymous", 15.00, POESIA, 5},
-        {33, "Essays", "Michel de Montaigne", 20.00, ENSAYO, 4},
-        {34, "Self-Reliance and Other Essays", "Ralph Waldo Emerson", 9.00, ENSAYO, 9},
-        {35, "Civil Disobedience and Other Essays", "Henry David Thoreau", 7.50, ENSAYO, 11},
-        {36, "Meditations", "Marcus Aurelius", 11.99, ENSAYO, 8},
-        {37, "The Federalist Papers", "Alexander Hamilton, James Madison, John Jay", 18.00, ENSAYO, 5},
-        {38, "The Communist Manifesto", "Karl Marx and Friedrich Engels", 5.99, ENSAYO, 12},
-        {39, "The Republic", "Plato", 16.00, ENSAYO, 6},
-        {40, "Thus Spoke Zarathustra", "Friedrich Nietzsche", 14.99, ENSAYO, 10}
-        };
- 
+        printBook(&libros[0],1, "To Kill a Mockingbird", "Harper Lee", 15.99,FICCION, 10);
+        printBook(&libros[1],2, "1984", "George Orwell", 12.49,FICCION, 5);
+        printBook(&libros[2],3, "The Great Gatsby", "F. Scott Fitzgerald", 10.99,FICCION, 8);
+        printBook(&libros[3],4, "Moby Dick", "Herman Melville", 18.99,FICCION, 12);
+        printBook(&libros[4],5, "War and Peace", "Leo Tolstoy", 20.00,FICCION, 7);
+        printBook(&libros[5],6, "Pride and Prejudice", "Jane Austen", 14.99,FICCION, 9);
+        printBook(&libros[6],7, "The Catcher in the Rye", "J.D. Salinger", 10.00,FICCION, 6);
+        printBook(&libros[7],8, "The Odyssey", "Homer", 17.49,FICCION, 4);
+        printBook(&libros[8],9, "Ulysses", "James Joyce", 25.00,FICCION, 2);
+        printBook(&libros[9],10, "The Divine Comedy", "Dante Alighieri", 22.00, POESIA, 3);
+        printBook(&libros[10],11, "Leaves of Grass", "Walt Whitman", 13.00, POESIA, 11);
+        printBook(&libros[11],12, "The Iliad", "Homer", 18.50,FICCION, 7);
+        printBook(&libros[12],13, "A Brief History of Time", "Stephen Hawking", 15.00, NO_FICCION, 15);
+        printBook(&libros[13],14, "The Art of War", "Sun Tzu", 9.99, NO_FICCION, 20);
+        printBook(&libros[14],15, "Sapiens: A Brief History of Humankind", "Yuval Noah Harari", 16.49, NO_FICCION, 13);
+        printBook(&libros[15],16, "The Selfish Gene", "Richard Dawkins", 14.00, NO_FICCION, 6);
+        printBook(&libros[16],17, "The Road to Serfdom", "F.A. Hayek", 10.50, NO_FICCION, 5);
+        printBook(&libros[17],18, "The Wealth of Nations", "Adam Smith", 30.00, NO_FICCION, 8);
+        printBook(&libros[18],19, "On the Origin of Species", "Charles Darwin", 24.99, NO_FICCION, 4);
+        printBook(&libros[19],20, "The Prince", "Niccolò Machiavelli", 8.99, NO_FICCION, 14);
+        printBook(&libros[20],21, "Hamlet", "William Shakespeare", 11.50, TEATRO, 6);
+        printBook(&libros[21],22, "Macbeth", "William Shakespeare", 9.50, TEATRO, 8);
+        printBook(&libros[22],23, "Othello", "William Shakespeare", 10.99, TEATRO, 7);
+        printBook(&libros[23],24, "A Doll's House", "Henrik Ibsen", 12.50, TEATRO, 5);
+        printBook(&libros[24],25, "Waiting for Godot", "Samuel Beckett", 13.99, TEATRO, 4);
+        printBook(&libros[25],26, "Death of a Salesman", "Arthur Miller", 14.99, TEATRO, 10);
+        printBook(&libros[26],27, "The Glass Menagerie", "Tennessee Williams", 11.00, TEATRO, 9);
+        printBook(&libros[27],28, "Long Day's Journey into Night", "Eugene O'Neill", 19.50, TEATRO, 3);
+        printBook(&libros[28],29, "The Importance of Being Earnest", "Oscar Wilde", 8.50, TEATRO, 15);
+        printBook(&libros[29],30, "The Waste Land", "T.S. Eliot", 6.99, POESIA, 10);
+        printBook(&libros[30],31, "Paradise Lost", "John Milton", 12.00, POESIA, 7);
+        printBook(&libros[31],32, "Beowulf", "Anonymous", 15.00, POESIA, 5);
+        printBook(&libros[32],33, "Essays", "Michel de Montaigne", 20.00, ENSAYO, 4);
+        printBook(&libros[33],34, "Self-Reliance and Other Essays", "Ralph Waldo Emerson", 9.00, ENSAYO, 9);
+        printBook(&libros[34],35, "Civil Disobedience and Other Essays", "Henry David Thoreau", 7.50, ENSAYO, 11);
+        printBook(&libros[35],36, "Meditations", "Marcus Aurelius", 11.99, ENSAYO, 8);
+        printBook(&libros[36],37, "The Federalist Papers", "Alexander Hamilton, James Madison, John Jay", 18.00, ENSAYO, 5);
+        printBook(&libros[37],38, "The Communist Manifesto", "Karl Marx and Friedrich Engels", 5.99, ENSAYO, 12);
+        printBook(&libros[38],39, "The Republic", "Plato", 16.00, ENSAYO, 6);
+        printBook(&libros[39],40, "Thus Spoke Zarathustra", "Friedrich Nietzsche", 14.99, ENSAYO, 10);    
+    
 
+
+/*
+Book * catalogo;
+catalogo = (Book*) malloc (40 * sizeof (Book));
+inicializarLibro(&catalogo[0], 1, "1984","George",...);
+inicializarLibro(&catalogo[1], 1, "1984","George",...);
+
+
+inicializarLibro(&catalogo[39], 40, "1984","George",...);
+
+
+void inicializar Libro (Book * dir,...)
+dir->id=
+dir->precio=
+strcpy(dir->autor,autor);
+*/
 
 printf("Lista de argumentos (hay %d argumentos):\n",argc);
     for(int i = 0; i<argc; i++){
@@ -196,7 +209,7 @@ else if (argc == 2){
     if (strcmp(argv[1], "mostrar")== 0)
     {
         int id = atoi(argv[2]);
-        libroPorId(id,libros);
+        libroPorId(libros,id);
     } else if (strcmp(argv[1], "autor")== 0)
     {
         printf("Llamar a buscar por autor\n");
