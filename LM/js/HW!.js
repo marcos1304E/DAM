cb0a9b5b720d25196aa27fbcd67d73ebf8f6ea7d
Marcos1304E <marcosescamilla1304@gmail.com>
2025-03-06 23:03:16 +0100

    js

---
+++ LM/js/HW!.js
@@ -0,0 +1,8 @@
+console.log("Hello, World!");
+alert("Hello, World!");
+document.write("Hello, World!");
+
+let numero = 10;
+console.log("El numero es: ",numero);//debugging
+console.log(numero);//debugging
+console.log(numero+10);//debugging
