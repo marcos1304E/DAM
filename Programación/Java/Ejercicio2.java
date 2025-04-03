36b33d40f92e404dec82d7f1da2dd91219e20672
marcos1304E <marcosescamilla1304@gmail.com>
2025-04-03 10:30:59 +0200

    java

---
+++ Programación/Java/Ejercicio2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Introduce un numero");
+		int n = entrada.nextInt();
+
+
+		int contador = 0;
+
+
+		do {
+
+			contador ++;
+			n /= 10;
+
+		}while(n > 0);
+
+		System.out.println("El número tiene " + contador + " dígitos.");
+
+		entrada.close();
+
+	}
+
+
+}
