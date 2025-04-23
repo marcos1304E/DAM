95847b4b61041b212fc407d3475a7965c35d862b
marcos1304E <marcosescamilla1304@gmail.com>
2025-04-23 09:59:12 +0200

    eventos

--- LM/js/tatiana.js
+++ LM/js/tatiana.js
@@ -10,15 +10,20 @@ function cambiarFondo(){
 function cambiarTexto(){
     document.body.innerHTML = "<h1>Hola Mundo</h1>";
 }
+
 function load(){
     let boton = document.querySelector("button");
     let boton2 = document.getElementById("boton2");
+    let boton3 = document.getElementById("boton3");
     let cuadroTexto = document.getElementById("cuadroTexto");
 
 //Eventos
     boton.addEventListener("click", cambiarFondo, false);
     boton2.addEventListener("click", cambiarTexto, false);
-    cuadroTexto.addEventListener("keydown", (event) => ());
+    boton3.addEventListener("click", (event) => console.log(event));
+    
+    cuadroTexto.addEventListener("keydown", (event) => {});
+    
 }
 
 window.addEventListener("DOMContentLoaded", load, false);
