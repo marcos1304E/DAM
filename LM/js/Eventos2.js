a64b2dd74fa5899872f2a50e124449668865e7f9
marcos1304E <marcosescamilla1304@gmail.com>
2025-04-28 12:00:09 +0200

    js

--- LM/js/Eventos2.js
+++ LM/js/Eventos2.js
@@ -16,7 +16,39 @@ function cuentaAtras(){
     
 }
 
+function rellenarTabla(){
+    let numero=9;
+    for(let i=1; i<=numero; i++){
+      document.getElementById(`${i}`).textContent = i;
+    }
+}
+
+function rellenarTabla2(){
+    let celda = document.querySelectorAll(`#tablaSegunda td`);
+    celda.forEach((td,i) => td.textContent=i+1);
+    
+}
+
+/*function palabras(){
+    
+    let palabras = ["Hola", "Adios", "Bienvenido", "HastaLuego", "Gracias"];
+    let palabraAleatoria = palabras[Math.floor(Math.random() * palabras.length)];
+    let contador = 0;
+    
+}
+*/
+
+function ahorcado(event){
+    let palabras = ["perro", "gato", "tortuga", "pez", "caiman"];
+    let palabrasSeleccionadas = palabras[random(palabras.length)];
+
+    console.log(event.target.textContent);
+
+}
+
+
 function load(){
+            //ej1
     let botonNumero = document.getElementById("numeroAleatorio");
         botonNumero.addEventListener("click", () => {
     let numero = Math.floor(Math.random() * 100);
@@ -24,8 +56,17 @@ function load(){
         }
             );
 
-let botonAtras = document.getElementById("cuentaAtras");
+            //ej2
+    let botonAtras = document.getElementById("cuentaAtras");
     botonAtras.addEventListener("click", cuentaAtras);
+            //ej3
+    document.getElementById("tabla").addEventListener("click", rellenarTabla);
+    document.getElementById("tabla2").addEventListener("click", rellenarTabla2);
+
+
+    let letras = document.querySelectorAll(".ejercicio5");
+    letras.forEach(botonesLetras => botonesLetras.addEventListener('click', ahorcado));
+
 }
 
 window.addEventListener("DOMContentLoaded", load, false);
