822049658d4cc08eb0a835ce57282929b461f8eb
marcos1304E <marcosescamilla1304@gmail.com>
2025-05-21 21:40:44 +0200

    Create Prestamo.java

---
+++ Programación/Java/ejercicio3/Prestamo.java
@@ -0,0 +1,44 @@
+public class Prestamo {
+
+	private int idArticulo;
+	private String fechaPrestamo;
+	private String fechaDevolucion;
+	private int numeroPrestamo;
+	
+	
+	
+	public int getIdArticulo() {
+		return idArticulo;
+	}
+	
+	public void setIdArticulo(int idArticulo) {
+		this.idArticulo = idArticulo;
+	}
+	
+	public String getFechaPrestamo() {
+		return fechaPrestamo;
+	}
+	
+	public void setFechaPrestamo(String fechaPrestamo) {
+		this.fechaPrestamo = fechaPrestamo;
+	}
+	
+	public String getFechaDevolucion() {
+		return fechaDevolucion;
+	}
+	
+	public void setFechaDevolucion(String fechaDevolucion) {
+		this.fechaDevolucion = fechaDevolucion;
+	}
+	
+	public int getNumeroPrestamo() {
+		return numeroPrestamo;
+	}
+	
+	public void setNumeroPrestamo(int numeroPrestamo) {
+		this.numeroPrestamo = numeroPrestamo;
+	}
+	
+	
+	
+}
